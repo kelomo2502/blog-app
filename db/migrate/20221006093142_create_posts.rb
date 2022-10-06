7a9e646d04f8f20cd45be62e0d861907dd803511
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-10-06 11:26:31 +0100

    Fix test issues

--- db/migrate/20221006093142_create_posts.rb
+++ db/migrate/20221006093142_create_posts.rb
@@ -1,12 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.bigint :author_id
-      t.text :title
-      t.integer :comments_counter
-      t.integer :likes_counter
+      t.text :text
+      t.string :title
+      t.integer :comments_counter, default: 0, null: false
+      t.integer :likes_counter, default: 0, null: false
       t.references :author, foreign_key: { to_table: 'users' }
-
       t.timestamps
     end
   end
